09cfb4573550c46fd756b28e7fe230505529cb00
Anastasia Shcherban <anastasia.shcherban@helsinki.fi>
2023-02-09 10:00:03 +0000

    Updated input generation script to account for merged minimal and cohort data.

--- scripts/merge_basic_info.py
+++ scripts/merge_basic_info.py
@@ -11,27 +11,23 @@ def main():
     parser = argparse.ArgumentParser(description='Script for preparing basic info phenotype file.')
     parser.add_argument('-s', '--sample_list', help='sample list extracted from fgpass factory samples', required=True)
     parser.add_argument('-d', '--death_extracted', help='death field extracted from endpoint', required=True)
-    parser.add_argument('-m', '--minimum', help='minimum endpoint', required=True)
-    parser.add_argument('-c', '--cohort', help='cohort data', required=True)
+    parser.add_argument('-m', '--minimum', help='minimum endpoint data containing cohort column', required=True)
     parser.add_argument('-a', '--array_samples', help='fg factory pass samples', required=True)
     parser.add_argument('-o', '--out_path', help='output path', required=True)
     args = vars(parser.parse_args())
 
-
     gt_samples = pd.read_csv(args["sample_list"], names=['FINNGENID'])
     death = pd.read_csv(args["death_extracted"], sep='\t')
-    minimum = pd.read_csv(args["minimum"], sep='\t', usecols=['FINNGENID', 'BL_YEAR', 'BL_AGE', 'SEX', 'regionofbirthname'])
-    cohort = pd.read_csv(args["cohort"], names=['FINNGENID', 'cohort'], sep='\t')
+    minimum = pd.read_csv(args["minimum"], sep='\t', usecols=['FINNGENID', 'BL_YEAR', 'BL_AGE', 'SEX', 'regionofbirthname', 'COHORT'])
+    minimum = minimum.rename(columns={"COHORT": "cohort"})
     array = pd.read_csv(args["array_samples"], sep=':')
-
 
     data = gt_samples.merge(death, how='left', on='FINNGENID',suffixes=(None, "_y"))
     data = data.merge(minimum, how='left', on='FINNGENID',suffixes=(None, "_y"))
-    data = data.merge(cohort, how='left', on='FINNGENID',suffixes=(None, "_y"))
     data = data.merge(array, how='left', on='FINNGENID',suffixes=(None, "_y"))
     data['AGE_AT_DEATH_OR_NOW'] = np.where(data['DEATH']==1, data['DEATH_AGE'], data['BL_AGE'] + datetime.now().year - data['BL_YEAR'])
     data = data[['FINNGENID', 'DEATH', 'SEX', 'AGE_AT_DEATH_OR_NOW', 'regionofbirthname', 'cohort', 'BATCH', 'CHIP']]
-
+    
     data = data.round({'AGE_AT_DEATH_OR_NOW': 1})
     data['DEATH'] = data['DEATH'].astype('Int64')
     data['SEX'] = np.where(data['SEX'] == 'female', 1, 0)
