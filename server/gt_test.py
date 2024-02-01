c5294e27a6bf969b3f762416fc66fd1941d1191b
Anastasia Shcherban <anastasia.shcherban@helsinki.fi>
2024-02-01 13:58:48 +0000

    Updated gt integration test file

--- server/gt_test.py
+++ server/gt_test.py
@@ -29,7 +29,6 @@ except Exception as e:
 config = {key: getattr(_conf_module, key) for key in dir(_conf_module) if not key.startswith('_')}
 fetch = Datafetch(config)
 
-
 def main():
 
     # parse the arguments
@@ -54,42 +53,55 @@ def main():
     filters = json.loads(args['filters'])
     save_output = args['save']
 
-    count = 0
-    success = True
-    if data_type == 'both' or data_type == 'chip':
-        filters['data_type'] = "chip"
-        res = getData(variant, filters, "chip")
-        if res[0] is None and res[1] is None:
-            print("Genotype data not found.")
-        else:
-            print("Genotype data extracted.")
-            eq = assert_equal_df(res)
-            success = False if not eq else True
-            if not eq and save_output:
-                get_nonequal_and_save(res, variant, filters)
-        count += 1
+    print("Filters to be used:", filters)
 
-    if data_type == 'both' or data_type == 'imputed':
-        filters['data_type'] = "imputed"
-        res = getData(variant, filters, "imputed")
-        if res[0] is None and res[1] is None:
-            print("Genotype data not found.")
-        else:
-            print("Genotype data extracted.")
-            eq = assert_equal_df(res)
-            success = False if not eq else True
-            if not eq and save_output:
-                get_nonequal_and_save(res, variant, filters)
-        count += 1
+    if data_type == 'both':
+        run_asserts(variant, "imputed", filters)
+        run_asserts(variant, "chip", filters)
 
-    end = time.time()
-
-    print("\n--------------------------------------------------")
-    print("Ran %s tests in %ss.\n" % (count, round(end - start, 4)))
-    if success:
-        print('OK')
+    elif data_type == 'imputed':
+        run_asserts(variant, "imputed", filters)    
+    
     else:
-        print('Fail')
+        run_asserts(variant, "chip", filters)
+
+
+def run_asserts(variant, data_type, filters):
+
+    filters['data_type'] = data_type
+    res = getData(variant, filters, data_type)
+    v = variant.split('-')
+    chips = fetch._get_chips(v[0], v[1], v[2], v[3])
+
+    vcf = res[0].loc[list(res[1].index), :].copy()
+    gb = res[1].copy()
+
+    print(f"\t{len(chips)} - chips (batches) for the variant found - showing first 3: {', '.join(list(chips)[0:3])}...")
+    print(f"\t{len(res[0])} - total samples extracted directly from vcf")
+    print(f"\t{len(vcf)} - subsampled only individuals that are listed in gb write output - compare gt data for those")
+    print(f"\t{len(res[1])} - samples from gb written data extracted")
+
+    try:
+        if filters['impchip'] == 'chip':
+            if len(chips) == 0:
+                assert gb.empty, f"impchip filter is set to `chip` and chips are empty but there is still some output of len {len(gb)}"
+            else:
+                if len(res[0]) != 0:
+                    assert not gb.empty, f"impchip filter is set to `chip` and {len(chips)} chips are found for a given variant but {len(gb)} samples are extracted"
+        elif filters['impchip'] == 'imp':
+            if len(chips) == 0:
+                assert_equal_df(vcf, gb, f"impchip filter is set to `imp` and chips are empty but genotypes extracted from vcf and gb differ")
+            else:
+                assert not gb.empty, f"impchip filter is set to `imp` and {len(chips)} chips are found for a given variant but {len(gb)} samples are extracted"
+        else:
+            assert_equal_df(vcf, gb, f"un-equal data extracted from vcf ({len(vcf)}) and the gb browser ({len(gb)}) for a given variant")
+        if vcf.empty:        
+            assert gb.empty, f"no data extracted for a given variant from vcf, but there are {len(gb)} samples extracted from the gb"
+    except Exception as e:
+        print(f"ERROR :: {e}")
+        print(" --------------------------------- FAIL ---------------------------------\n")
+    else:
+        print(" -------------------------- All asserts PASSED --------------------------\n")
 
 
 def get_nonequal_and_save(dfs, variant, filters):
@@ -112,12 +124,12 @@ def get_nonequal_and_save(dfs, variant, filters):
     print("Saved to", fout)
 
 
-def assert_equal_df(dfs):    
+def assert_equal_df(df1, df2, error_message_additional):    
     # assert data frames are equal
     try:
-        assert_frame_equal(dfs[0], dfs[1], check_dtype=False)
+        assert_frame_equal(df1, df2, check_dtype=False)
     except AssertionError as e:
-        print("ERROR :: %s." % e)
+        print("ERROR :: %s. %s" % (e, error_message_additional))
         return False
     else:
         return True
@@ -141,11 +153,12 @@ def getData(variant, filters, data_type):
         try:
             response = fetch.write_variants(variant, filters.copy(), data_type)
         except ValueError as e:
-            dat_gb_full = None
+            dat_gb = pd.DataFrame()
         else:
             text = response.data
-            dat_gb_full = pd.read_csv(io.StringIO(text.decode()), sep='\t', low_memory=False)
-            dat_gb_full.index = list(dat_gb_full['FINNGENID'])
+            dat_gb = pd.read_csv(io.StringIO(text.decode()), sep='\t', low_memory=False)
+            dat_gb.index = list(dat_gb['FINNGENID'])
+            dat_gb = dat_gb[['FINNGENID', 'gt']]
         
         # extract genotypes from the vcf directly
         if data_type == 'imputed':
@@ -156,14 +169,14 @@ def getData(variant, filters, data_type):
         gt_data = x.fetch("chr%s" % chr_search, pos - 1, pos)
         gt = None
         for row in gt_data:
-            data = row.split('\t')      
+            data = row.split('\t')   
             if data[3] == ref and data[4] == alt:
                 gt = data
                 break
         
         # construct data frame containing genotypes extracted from vcf
         if gt is None:
-            dat_vcf = None
+            dat_vcf = pd.DataFrame()
         else:
             samples = x.header[len(x.header) - 1].split('\t')
             d = pd.DataFrame({'FINNGENID': samples, 'gt_': gt})
@@ -171,16 +184,8 @@ def getData(variant, filters, data_type):
             d['gt'] = [item.split(':')[0] for item in d['gt_']]
             dat_vcf = d[['FINNGENID', 'gt']]
             dat_vcf.index = list(dat_vcf['FINNGENID'])
-        
-        # re-order the dataframe containing genotypes extracted from vcf
-        # based on the sample order in the genotype browser's output
-        dat_gb = None
-        if dat_gb_full is not None:
-            dat_gb = dat_gb_full[['FINNGENID', 'gt']].copy()
-            if  dat_vcf is not None:
-                dat_vcf = dat_vcf.loc[list(dat_gb.index), :]
-    
-    return dat_vcf, dat_gb, dat_gb_full
+
+    return dat_vcf, dat_gb
 
 
 def str2bool(v):
