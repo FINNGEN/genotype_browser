17e0422ac4101d85eea742362520b2c2c3db8d74
Anastasia Shcherban <anastasia.shcherban@helsinki.fi>
2024-02-19 07:04:09 +0000

    - Added config template to be populated with newly generated data and used by the integration tests - Fixed get_chip_from_anno.sh to sync batch names in the prepared basic info and chip table (db) - Updated integration tests - Added R12 inputs for preparing GB inputs

--- scripts/get_chip_from_anno.sh
+++ scripts/get_chip_from_anno.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 annotated_variants_gz=$1 	# e.g. /mnt/nfs/r6/annotations/R6_annotated_variants_v1.gz
 output_chipvars_gz=$2 		# e.g. /mnt/nfs/r6/annotations/R6_chipvars.txt.gz
 if beginswith gs: "$annotated_variants_gz"; then
 	time gsutil cat "$annotated_variants_gz" | zcat -f | \
-    	awk 'BEGIN{OFS="\t"} NR==1{for (i=1;i<=NF;i++) if ($i ~ "^CHIP_") {sub("CHIP_", "", $i); a[i]=$i}} NR>1{for (i in a) if ($i==1) print $1,a[i]}' \
+    	awk 'BEGIN{FS=OFS="\t"} NR==1{ for (i=1;i<=NF;i++) if ($i ~ "^CHIP_") { split($i, s1, "."); split(s1[1], s2, "_chr"); h=s2[1]; sub("CHIP_", "", h); a[i]=h}} NR>1{ for (i in a) if ($i==1) print $1,a[i]}'  \
     	| bgzip -@4 > ${output_chipvars_gz}
 else
 	time zcat -f  "$annotated_variants_gz" | \
-        awk 'BEGIN{OFS="\t"} NR==1{for (i=1;i<=NF;i++) if ($i ~ "^CHIP_") {sub("CHIP_", "", $i); a[i]=$i}} NR>1{for (i in a) if ($i==1) print $1,a[i]}' \
+        awk 'BEGIN{FS=OFS="\t"} NR==1{ for (i=1;i<=NF;i++) if ($i ~ "^CHIP_") { split($i, s1, "."); split(s1[1], s2, "_chr"); h=s2[1]; sub("CHIP_", "", h); a[i]=h}} NR>1{ for (i in a) if ($i==1) print $1,a[i]}' \
         | bgzip -@4 > ${output_chipvars_gz}
 fi
